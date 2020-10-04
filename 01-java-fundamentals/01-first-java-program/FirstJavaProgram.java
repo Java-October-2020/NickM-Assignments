f00ce2eba562eab8ffc60651d1debf4b80ab9d63
Nick Miranda <nicolasamiranda97@gmail.com>
2020-10-03 22:11:05 -0400

    folders

---
+++ 01-java-fundamentals/01-first-java-program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My Name is Nick Miranda");
+        System.out.println("I am 23");
+        System.out.println("My hometown is Fortlauderdale, FL");
+    }
+}
