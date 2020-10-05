a118c3046d9bd4ca63c497ec0a557194a82ad4a6
Nick Miranda <nicolasamiranda97@gmail.com>
2020-10-04 21:17:36 -0400

    added Pythagorean program calculator

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args){
+        Pythagorean hypotenuse = new Pythagorean();
+        double calculate = hypotenuse.calculateHypotenuse(24,18);
+        System.out.println("the length of the Hypotenuse is: " + calculate);
+    }
+}
