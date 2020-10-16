0c4dcb7962478e8040bcbc74424afd0730f0e46a
Nick Miranda <nicolasamiranda97@gmail.com>
2020-10-16 00:13:40 -0400

    object master 1

---
+++ 02-java-oop/05-object-master-pt1/Human.java
@@ -0,0 +1,34 @@
+public class Human {
+    private int strength = 3;
+    private int intelligence = 3;
+    private int stealth = 3;
+    private int health = 100;
+
+    public Human(){
+
+    }
+
+    public void attack(Human target){
+        target.health -= this.strength;
+        System.out.println(target.getClass().getCanonicalName() + " health is now: " + health);
+    }
+    // public Human(int strength, int intelligence, int stealth, int health){
+    //     this.strength = strength;
+    //     this.intelligence = intelligence;
+    //     this.stealth = stealth;
+    //     this.health = health;
+    // }
+
+    public int getStrength(){
+        return this.strength;
+    }
+    public int getIntelligence(){
+        return this.intelligence;
+    }
+    public int getStealth(){
+        return this.stealth;
+    }
+    public int getHealth(){
+        return this.health;
+    }
+}
---
+++ 02-java-oop/05-object-master-pt1/HumanTest.java
@@ -0,0 +1,8 @@
+public class HumanTest {
+    public static void main(String[] args) {
+        Human john = new Human();
+        Human billy = new Human();
+
+        john.attack(billy);
+    }
+}
