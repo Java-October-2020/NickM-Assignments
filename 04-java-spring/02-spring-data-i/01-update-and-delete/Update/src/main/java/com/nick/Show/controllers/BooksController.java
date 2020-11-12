81de9cd2920e5228b32472d471ab92f79b0d93e2
Nick Miranda <nicolasamiranda97@gmail.com>
2020-11-12 18:23:13 -0500

    update and delete

---
+++ 04-java-spring/02-spring-data-i/01-update-and-delete/Update/src/main/java/com/nick/Show/controllers/BooksController.java
@@ -0,0 +1,76 @@
+package com.nick.Show.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.nick.Show.models.Book;
+import com.nick.Show.services.BookService;
+
+@Controller
+public class BooksController {
+    private final BookService bookService;
+    
+    public BooksController(BookService bookService) {
+        this.bookService = bookService;
+    }
+    
+    @RequestMapping("/books")
+    public String index(Model model) {
+        List<Book> books = bookService.allBooks();
+        model.addAttribute("books", books);
+        return "index.jsp";
+    }
+    @RequestMapping("/books/{index}")
+    public String findBookByIndex(Model model, @PathVariable("index") int index) {
+        Book book = bookService.findBookByIndex(index);
+        model.addAttribute("book", book);
+        return "showBook.jsp";
+    }
+    @RequestMapping("/books/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "newBook.jsp";
+    }
+    @RequestMapping(value="/books", method=RequestMethod.POST)
+    public String create(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "newBook.jsp";
+        } else {
+            bookService.createBook(book);
+            return "redirect:/books";
+        }
+    }
+    @RequestMapping("/books/edit/{id}")
+    public String editBook(@PathVariable("id") int id, Model model) {
+        Book book = bookService.findBookByIndex(id);
+        if (book != null){
+            model.addAttribute("book", book);
+            return "editBook.jsp";
+        }else{
+            return "redirect:/books";
+        }
+    }
+    @PostMapping("/books/edit/{id}")
+    public String updateBook(@PathVariable("id") int id, @Valid @ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "editBook.jsp";
+        }else{
+            bookService.updateBook(id, book);
+            return "redirect:/books";
+        }
+    }
+    @RequestMapping(value="/books/delete/{id}")
+    public String destroyBook(@PathVariable("id") Long id) {
+        bookService.destroyBook(id);
+        return "redirect:/books";
+    }
+}
---
+++ 04-java-spring/02-spring-data-i/01-update-and-delete/Update/src/main/java/com/nick/Show/services/BookService.java
@@ -0,0 +1,71 @@
+package com.nick.Show.services;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.nick.Show.models.Book;
+import com.nick.Show.repositories.BookRepository;
+
+@Service
+public class BookService {
+	
+    // adding the book repository as a dependency
+    private final BookRepository bookRepository;
+    
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+	
+    // initialize the books variable with values
+    private List<Book> books = new ArrayList<>(Arrays.asList(
+            new Book("Harry Potter and the Sorcerer's Stone", "A boy wizard saving the world", "english", 309),
+            new Book("The Great Gatsby", "The story primarily concerns the young and mysterious millionaire Jay Gatsby", "english", 180),
+            new Book("Moby Dick", "The saga of Captain Ahab", "english", 544),
+            new Book("Don Quixote", "Life of a retired country gentleman", "english", 150),
+            new Book("The Odyssey", "Ancient Greek epic poem", "english", 475)
+            ));
+    
+//    // returns all the books
+//    public List<Book> allBooks() {
+//        return bookRepository.findAll();
+//    }
+    public List<Book> allBooks() {
+        return books;
+    }
+    // finds book by key value
+    public Book findBookByIndex(int index) {
+        if (index < books.size()){
+            return books.get(index);
+        }else{
+            return null;
+        }
+    }
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+//    public void updateBook(int id, Book book) {
+//        if (id < books.size()){
+//            books.set(id, book);
+//        }
+//    }
+    public Book updateBook(Book updatedBook) {
+    	return this.bookRepository.save(updatedBook);
+    }
+ 
+	public void destroyBook(Long id) {
+      this.bookRepository.deleteById(id);
+    }
+}
